4fca96f88b84085764d3633f53f8fb86525d7ed4
Wal Agui <walfre.am@gmail.com>
2025-08-18 14:57:06 -0600

    LAyout sitaxis Styles fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,11 @@ import type { Config } from "tailwindcss";
 
 export default {
     darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
   	extend: {
